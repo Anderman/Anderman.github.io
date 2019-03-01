8e0f421abdf211e525dc519242dc47dcf224eb47
Thom Kiesewetter <tkiesewetter@live.com>
2019-03-01 14:19:02 +0100

    Fix Uzovicode

--- CodeProvider/Infrastructure/ExcelUzoviReader.cs
+++ CodeProvider/Infrastructure/ExcelUzoviReader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using ExcelDataReader;
 using Newtonsoft.Json;
+// ReSharper disable StringLiteralTypo
 
 namespace CodeProvider.Infrastructure
 {
@@ -10,6 +11,19 @@ namespace CodeProvider.Infrastructure
 	{
 		private const int Offset = 'O' - 'A';
 		private const string SheetName = "Concern";
+
+		private static string Clean(string value)
+		{
+			value= value.Replace("  ", " ");
+			value= value.Replace("DSW-STAD  HOLLAND", "Dsw-Stad Holland");
+			value= value.Replace("DE FRIESLAND", "De Friesland");
+			value= value.Replace("MULTIZORG", "Multizorg");
+			value= value.Replace("ALGEMEEN", "Algemeen");
+			value= value.Replace("ALG ", "Algemeen");
+			value= value.Replace("T.B.V.", "t.b.v.");
+			value= value.Replace("COOPERATIE", "Coöperatie");
+			return value;
+		}
 
 		public static string GetCodes(Stream stream)
 		{
@@ -26,7 +40,7 @@ namespace CodeProvider.Infrastructure
 					for (var i = 0; i < concerns.Length; i++)
 						if (reader.GetString(i + Offset) == "x")
 							concern = concerns[i].Replace("\n"," ").Trim();
-					uzoviCodes.Add(new UzoviRecord { Concern = concern, Description = name, Code = uzoviCode });
+					uzoviCodes.Add(new UzoviRecord { Concern = Clean(concern), Description = Clean(name), Code = uzoviCode });
 				}
 			}
 
